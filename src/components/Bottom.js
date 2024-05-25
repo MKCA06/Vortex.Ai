bf2d719335dd1fa1ec4b436ed456502968c4a4b1
MKCA06 <manikchadha0609@gmail.com>
2024-05-25 19:59:10 +0530

    Adding Animations

--- src/components/Bottom.js
+++ src/components/Bottom.js
@@ -5,8 +5,14 @@ import Progress from './Progress';
 import Table from './Table';
 import { BsFillPersonFill, BsEnvelope, BsFileText, BsCalendar } from 'react-icons/bs';
 import TopBar from './TopBar';
+import { useInView } from 'react-intersection-observer';
 
 const Bottom = () => {
+  const { ref: topBarRef, inView: topBarInView } = useInView({ triggerOnce: true, threshold: 0.1 });
+  const { ref: cardRef, inView: cardInView } = useInView({ triggerOnce: true, threshold: 0.1 });
+  const { ref: chartRef, inView: chartInView } = useInView({ triggerOnce: true, threshold: 0.1 });
+  const { ref: tableRef, inView: tableInView } = useInView({ triggerOnce: true, threshold: 0.1 });
+
   return (
     <div className="container-fluid bg-dark text-light">
       <div className="row">
@@ -16,63 +22,63 @@ const Bottom = () => {
         </div>
         {/* Content */}
         <div className="col-lg-10 col-md-9">
-          <TopBar />
+          <div ref={topBarRef} className={`${topBarInView ? 'animate__animated animate__fadeIn' : ''}`}>
+            <TopBar />
+          </div>
           {/* Cards */}
-          <div className="row mt-3">
-            <div className="col-lg-3 col-md-6">
+          <div className="row mt-3" ref={cardRef}>
+            <div className={`col-lg-3 col-md-6 ${cardInView ? 'animate__animated animate__fadeInUp' : ''}`}>
               <div className="card bg-dark text-light">
                 <div className="card-body">
                   <BsFillPersonFill style={{ fontSize: '2rem', borderRadius: '50%' }} />
-                  <p className="card-text">63</p>
+                  <p className="card-text m-1">63</p>
                   <p className="card-text">New Leads</p>
                 </div>
               </div>
             </div>
-            <div className="col-lg-3 col-md-6">
+            <div className={`col-lg-3 col-md-6 ${cardInView ? 'animate__animated animate__fadeInUp' : ''}`}>
               <div className="card bg-dark text-light">
                 <div className="card-body">
                   <BsEnvelope style={{ fontSize: '2rem', borderRadius: '50%' }} />
-                  <p className="card-text">25</p>
+                  <p className="card-text m-1">25</p>
                   <p className="card-text">Email</p>
                 </div>
               </div>
             </div>
-            <div className="col-lg-3 col-md-6">
+            <div className={`col-lg-3 col-md-6 ${cardInView ? 'animate__animated animate__fadeInUp' : ''}`}>
               <div className="card bg-dark text-light">
                 <div className="card-body">
                   <BsFileText style={{ fontSize: '2rem', borderRadius: '50%' }} />
-                  <p className="card-text">49</p>
+                  <p className="card-text m-1">49</p>
                   <p className="card-text">Proposal</p>
                 </div>
               </div>
             </div>
-            <div className="col-lg-3 col-md-6">
+            <div className={`col-lg-3 col-md-6 ${cardInView ? 'animate__animated animate__fadeInUp' : ''}`}>
               <div className="card bg-dark text-light">
                 <div className="card-body">
                   <BsCalendar style={{ fontSize: '2rem', borderRadius: '50%' }} />
-                  <p className="card-text">12</p>
+                  <p className="card-text m-1">12</p>
                   <p className="card-text">Appointments</p>
                 </div>
               </div>
             </div>
           </div>
           {/* Graphs and Charts */}
-          <div className="row mt-3">
-            <div className="col-lg-8">
+          <div className="row mt-3" ref={chartRef}>
+            <div className={`col-lg-8 ${chartInView ? 'animate__animated animate__fadeInLeft' : ''}`}>
               <div className="card bg-dark text-light">
                 <div className="card-body">
                   <div style={{ height: '300px', backgroundColor: '#333' }}>
-                    {/* Insert your graph component or code here */}
                     <BarChart />
                   </div>
                 </div>
               </div>
             </div>
-            <div className="col-lg-4">
+            <div className={`col-lg-4 ${chartInView ? 'animate__animated animate__fadeInRight' : ''}`}>
               <div className="card bg-dark text-light">
                 <div className="card-body">
-                  <div style={{ height: '300px', backgroundColor: '#333', display:"flex", justifyContent:"center", alignItems:"center" }}>
-                    {/* Insert your circular graph component or code here */}
+                  <div style={{ height: '300px', backgroundColor: '#333', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                     <Progress />
                   </div>
                 </div>
@@ -80,8 +86,8 @@ const Bottom = () => {
             </div>
           </div>
           {/* Table */}
-          <div className="row mt-3">
-            <div className="col">
+          <div className="row mt-3" ref={tableRef}>
+            <div className={`col ${tableInView ? 'animate__animated animate__fadeInUp' : ''}`}>
               <div className="card bg-dark text-light">
                 <div className="card-body">
                   <Table />
